8971d0651215a58c6f17dd154fa6ca35ee59acb1
amjadmoh1234 <163919971+amjadmoh1234@users.noreply.github.com>
2024-03-25 22:07:01 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginSignup from './components/Assets/loginsignup/LoginSignup';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
